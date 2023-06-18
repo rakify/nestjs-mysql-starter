66bf74db814473c44047cb7c913a4de744b87e37
rakiblh <rakib@lemonhivee.com>
2023-06-18 16:51:13 +0600

    update entity

---
+++ src/database/entity/user.entity.ts
@@ -0,0 +1,45 @@
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@ObjectType({ description: 'user entity' })
+@Entity({ name: 'User' })
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  @Field(() => Int)
+  ID: number;
+
+  @CreateDateColumn()
+  @Field(() => Date)
+  CreatedDate: Date;
+
+  @UpdateDateColumn()
+  @Field(() => Date)
+  UpdatedDate: Date;
+
+  @Column({ name: 'Name', nullable: false, comment: 'Name of the user' })
+  @Field()
+  Name: string;
+
+  @Column({
+    name: 'Email',
+    nullable: false,
+    comment: 'Provided email of the user',
+  })
+  @Field()
+  Email: string;
+
+  @Column({
+    name: 'Password',
+    type: 'text',
+    comment: 'hashed password of the user',
+    nullable: false,
+  })
+  @Field()
+  Password: string;
+}
