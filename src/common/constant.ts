50b9f41c6396cf291dad51df3c470f2d0cba3a72
rakiblh <rakib@lemonhivee.com>
2023-06-18 16:47:19 +0600

    add constant

---
+++ src/common/constant.ts
@@ -0,0 +1,28 @@
+export const constant = {
+  USER_ALREADY_EXIST: 'provided email address already exist in system.',
+  HASH_SALT_COUNT: 10,
+  EMAIL_NOT_FOUND:
+    'Sorry, provided email address does not exist in the system.',
+  PROVIDED_WRONG_PASSWORD: 'Provided password is in-correct please try again?',
+  LOGIN_SUCCESSFUL: 'You are login successfully.',
+  UNAUTHORIZED_ACCESS_MESSAGE: 'Please login first to perform this operation.',
+  LOGOUT_SUCCESSFUL:
+    'You have been logout, please login again to gain the access.',
+  PRODUCT_NOT_FOUND: 'Sorry, requested product not found.',
+  ONLY_OWNER_OF_THE_PRODUCT_CAN_PERFORM_THIS_ACTION:
+    'Sorry, only product owner can perform this action.',
+  WEAK_PASSWORD_MESSAGE: 'Sorry your password is too weak.',
+  INVALID_NAME_RANGE_MESSAGE: 'Please provide name between 3 to 45 characters.',
+  INVALID_EMAIL_FORMAT: 'Please provide proper email address.',
+  PLEASE_ENTER_THE_PRODUCT_NAME:
+    'Please enter the product name first to create the product.',
+  INVALID_PRODUCT_NAME:
+    'Please enter correct character name to create the product.',
+  PRICE_POSITIVE: 'Please provide positive price for the product.',
+  IN_STOCK_BOOLEAN: 'In stock value should be true or false',
+  TRANSACTION_ID_LENGTH: 5,
+  USER_DOES_NOT_EXIST: 'User does not exist in the database',
+  YOU_ALREADY_BUY_THIS_ORDER: 'You already purchased this item',
+  PURCHASED_ORDER_SUCCESS_MESSAGE:
+    'Congratulations!, you have successfully place your order',
+};
