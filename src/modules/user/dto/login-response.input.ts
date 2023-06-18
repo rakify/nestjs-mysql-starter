f904f9d8217e99108b8385a4e2923ddbfc86a829
rakiblh <rakib@lemonhive.com>
2023-06-18 22:58:46 +0600

    UserDecoderData can be null

--- src/modules/user/dto/login-response.input.ts
+++ src/modules/user/dto/login-response.input.ts
@@ -13,6 +13,6 @@ export class loginResponseDTO {
   @Field()
   loginMessage: string;
 
-  @Field(() => UserDecoderData)
+  @Field(() => UserDecoderData, { nullable: true })
   userData: UserDecoderData;
 }
