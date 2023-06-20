84fc790b166fea322b5736b495f610dd5cacd4e2
rakiblh <rakib@lemonhivee.com>
2023-06-20 15:13:37 +0600

    fix: auth issue

--- src/modules/auth/auth.resolver.ts
+++ src/modules/auth/auth.resolver.ts
@@ -1,16 +1,18 @@
 import { AuthService } from './auth.service';
-import { Args, Mutation, Resolver } from '@nestjs/graphql';
-import { Public } from './auth.guard';
+import { Args, Mutation, Resolver, Query } from '@nestjs/graphql';
 import { RegisterResponseDTO } from './dto/register-response.input';
+import { LoggedInUserData, LoginResponseDTO } from './dto/login-response.input';
 import { RegisterUserDTO } from './dto/register-user.input';
-import { LoginResponseDTO } from './dto/login-response.input';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from './guards/auth.guard';
+import { CurrentUser } from './guards/current-user.decoder';
+import { UserEntity } from 'modules/user/user.entity';
 
 @Resolver()
 export class AuthResolver {
   constructor(private authService: AuthService) {}
 
   // to register the user into the system
-  @Public()
   @Mutation(() => RegisterResponseDTO, {
     description: 'to register user into the system',
   })
@@ -20,7 +22,6 @@ export class AuthResolver {
     return this.authService.registerUser(registerUser);
   }
 
-  @Public()
   @Mutation(() => LoginResponseDTO, {
     description: 'to login user into the system',
   })
@@ -29,5 +30,11 @@ export class AuthResolver {
     @Args('password') password: string,
   ): Promise<LoginResponseDTO> {
     return this.authService.signIn(email, password);
+  }
+
+  @UseGuards(AuthGuard)
+  @Query(() => LoggedInUserData)
+  async getLoggedUser(@CurrentUser() user: UserEntity) {
+    return this.authService.getCurrentUser(user);
   }
 }
