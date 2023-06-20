479a1a8f74f8d5c1054801e653f71e93ea5a0eca
rakiblh <rakib@lemonhivee.com>
2023-06-20 16:33:49 +0600

    chore: clear unnecessary code

--- src/core/helper/index.ts
+++ src/core/helper/index.ts
@@ -1,6 +1,5 @@
 import * as bcrypt from 'bcrypt';
 import { constant } from '../default';
-import { BadRequestException } from '@nestjs/common';
 
 export interface IGenerateToken {
   ID: number;
@@ -14,29 +13,4 @@ const hashPassword = (plainPassword: string) =>
 const comparePassword = (plainPassword: string, hashPassword: string) =>
   bcrypt.compare(plainPassword, hashPassword);
 
-const formatter = (value: number) => {
-  const stringValue = Number(value).toFixed(2);
-  return Number(stringValue);
-};
-
-/* 
-   manual validation due to below issue 
-   reference https://github.com/nestjs/passport/issues/129 
-*/
-const emailAndPasswordValidation = (email: string, password: string) => {
-  // validate email
-  const emailRegex =
-    /^[-!#$%&'*+\/0-9=?A-Z^_a-z{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/;
-  const emailValidationResult = emailRegex.test(email);
-  if (!emailValidationResult) {
-    throw new BadRequestException(constant.INVALID_EMAIL_FORMAT);
-  }
-  // manual password validation
-  const passwordRegex = /^.{4,}$/;
-  const passwordValidationResult = passwordRegex.test(password);
-  if (!passwordValidationResult) {
-    throw new BadRequestException(constant.WEAK_PASSWORD_MESSAGE);
-  }
-};
-
-export { hashPassword, comparePassword, formatter, emailAndPasswordValidation };
+export { hashPassword, comparePassword };
