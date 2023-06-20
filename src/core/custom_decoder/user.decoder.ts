e81ad54b0a062fb3b09772b28a0d8078bf660299
rakiblh <rakib@lemonhivee.com>
2023-06-20 15:12:09 +0600

    chore: delete unnecessary code

--- src/core/custom_decoder/user.decoder.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { GqlExecutionContext } from '@nestjs/graphql';
-
-export const User = createParamDecorator(
-  (data: string, ctx: ExecutionContext) => {
-    const user = GqlExecutionContext.create(ctx).getContext().req.user;
-    return data ? user[data] : user;
-  },
-);
