f6ff2356d578a2c2c1078e557fe6f30c752c544e
rakiblh <rakib@lemonhivee.com>
2023-06-22 14:44:38 +0600

    fix: update validation roles

--- src/modules/user/dto/update-user-personal-info.input.ts
+++ src/modules/user/dto/update-user-personal-info.input.ts
@@ -1,5 +1,6 @@
 import { Field, InputType } from '@nestjs/graphql';
 import {
+  IsDefined,
   IsEmail,
   IsString,
   IsUrl,
@@ -12,6 +13,10 @@ import {
 export class UpdateUserPersonalInfoInput {
   @Field({ nullable: true, description: 'user input value for Email' })
   @ValidateIf((_, value) => value !== undefined)
+  @IsDefined({
+    // if it is defined but as null
+    message: 'Email can not be set as null',
+  })
   @IsEmail()
   @MaxLength(100, {
     message: 'Email address is too long',
@@ -20,12 +25,15 @@ export class UpdateUserPersonalInfoInput {
 
   @Field({ nullable: true, description: 'user input value for Password' })
   @ValidateIf((_, value) => value !== undefined)
-  @IsString()
+  @IsDefined({
+    // if it is defined but as null
+    message: 'Password can not be set as null',
+  })
   @MinLength(4, {
-    message: 'Password is too short',
+    message: 'Password must contain atleast 4 characters',
   })
   @MaxLength(50, {
-    message: 'Password is too long',
+    message: 'Password can not be greater than 50 characters',
   })
   password: string;
 
@@ -33,10 +41,10 @@ export class UpdateUserPersonalInfoInput {
   @ValidateIf((_, value) => value !== undefined && value !== null)
   @IsString()
   @MinLength(2, {
-    message: 'First name is too short',
+    message: 'First name must contain at least 2 characters',
   })
   @MaxLength(50, {
-    message: 'First name is too long',
+    message: 'First name can not contain more than 50 characters',
   })
   firstName: string;
 
@@ -44,26 +52,26 @@ export class UpdateUserPersonalInfoInput {
   @ValidateIf((_, value) => value !== undefined && value !== null)
   @IsString()
   @MinLength(2, {
-    message: 'Last name is too short',
+    message: 'Last name must contain at least 2 characters',
   })
   @MaxLength(50, {
-    message: 'Last name is too long',
+    message: 'Last name can not contain more than 50 characters',
   })
   lastName: string;
 
   @Field({ nullable: true, description: 'user input value for avatarLink' })
   @ValidateIf((_, value) => value !== undefined && value !== null)
   @IsUrl()
-  @MaxLength(200, {
-    message: 'Link is too long',
+  @MaxLength(400, {
+    message: 'Avatar Link is too long',
   })
   avatarLink: string;
 
   @Field({ nullable: true, description: 'user input value for coverLink' })
   @ValidateIf((_, value) => value !== undefined && value !== null)
   @IsUrl()
-  @MaxLength(200, {
-    message: 'Link is too long',
+  @MaxLength(400, {
+    message: 'Cover Link is too long',
   })
   coverLink: string;
 }
