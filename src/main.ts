d67bcb56075dccc70c012b0f03fabe7d543b0af8
rakiblh <rakib@lemonhivee.com>
2023-06-22 13:59:12 +0600

    feat: add validationPipe globally

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { connectDataSource } from 'core/config/datasource.config';
 import { PORT } from 'core/environments';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
   await Promise.all([app.listen(PORT || 4000), connectDataSource()]);
 }
 bootstrap();
